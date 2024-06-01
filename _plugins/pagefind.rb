10f73dc7ef80faeb828b7f1eb6052a9fda884c53
Helena Rasche <hexylena@galaxians.org>
2024-06-01 17:47:14 +0200

    handle running in gh

--- _plugins/pagefind.rb
+++ _plugins/pagefind.rb
@@ -1,5 +1,12 @@
 Jekyll::Hooks.register :site, :post_write do |site|
   Jekyll.logger.info "Running pagefind"
   # p "#{site.baseurl}"
-  `npm exec -- pagefind --site _site --output-path _site/recipes/pagefind/`
+  # if github_run_id is set, we are running in github actions
+  puts "#{ENV}"
+  if ENV['GITHUB_RUN_ID']
+    p "Running in github actions"
+    `npm exec -- pagefind --site _site --output-path _site/pagefind/`
+  else
+    `npm exec -- pagefind --site _site --output-path _site/recipes/pagefind/`
+  end
 end
